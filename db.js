f39295833214805f470adba26e9df8f5c72273a4
Shreekanth000001 <shreekanth.k000001@gmail.com>
2024-06-29 17:52:52 +0530

    Updated db.js to previous state

--- db.js
+++ db.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb+srv://shreekanthk000001:tms1BvMs8wnDgLv2@oneddb.ao8aqdv.mongodb.net/';
 
-
 const connecToMongo = async () => {
-    try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+    const conn = await mongoose.connect(mongoURI)
+        .then((result) => {
+            console.log('success');
+        }).catch((err) => {
+            console.log('no success', err);
         });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-    }
-};
+
+
+
+}
 
 
 module.exports = connecToMongo
